b7cac728527aaef38bc500f7fa9f3d1bd0009a74
wonny-Jo <60210088+wonny-Jo@users.noreply.github.com>
2021-05-13 23:48:03 +0900

    plant app upload

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/components/featurred_plants.dart';
+import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
+import 'package:plant_app/screens/home/components/recomend_plants.dart';
+import 'package:plant_app/screens/home/components/title_with_more_bbtn.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
